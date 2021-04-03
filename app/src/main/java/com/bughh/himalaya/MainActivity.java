8e6045c75958b34019c8e3e2384b0c634e57f3b0
bughh <bugphh@gmail.com>
2021-04-03 21:07:34 +0800

    集成喜马拉雅 SDK, 并测试

--- app/src/main/java/com/bughh/himalaya/MainActivity.java
+++ app/src/main/java/com/bughh/himalaya/MainActivity.java
@@ -3,12 +3,46 @@ package com.bughh.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 获取喜马拉雅分类内容
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    Log.d(TAG, "category size: " + categories.size());
+                    for (Category category : categories) {
+                        Log.d(TAG, "category name " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "error code: " + i + " error msg: " + s);
+            }
+        });
     }
+
+
 }
